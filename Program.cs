d7234d1272056032ef7285e590f2c3b0cb4e630e
OskartuCZ <osky.tuzka@gmail.com>
2023-11-23 09:56:21 +0100

    Initial commit”

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool proceed = true;
+
+while (proceed)
+{
+    string result = "";
+    Console.WriteLine("Zadejte slovo:");
+    string userInput = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(userInput))
+    {
+        proceed = false;
+    }
+    result += userInput;
+    //Console.WriteLine($"Debug: {result}");
+}
+Console.WriteLine("Zde je cílový výpis!");
